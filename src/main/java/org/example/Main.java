b6a12fc977f4fbda2ec379de94725e5c0776db78
NazarMandryk <nazarmandryk4@gmail.com>
2023-03-02 21:39:21 +0200

    firstCommit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+import x1.lib1; //наша бібліотека
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+    }
+}
